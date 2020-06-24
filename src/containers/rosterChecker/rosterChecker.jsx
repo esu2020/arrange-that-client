2bd09e3ba20637b3bfba71553472a3da76437cc5
Eva <esu@berkeley.edu>
2020-06-24 10:29:52 -0700

    change left spacing

--- src/containers/rosterChecker/rosterChecker.jsx
+++ src/containers/rosterChecker/rosterChecker.jsx
@@ -39,7 +39,7 @@ const styles = theme => ({
         background: '#fafafa',
     },
     cardHeader: {
-        paddingLeft: 10,
+        paddingLeft: 50,
         paddingTop: 10,
         paddingBottom: 0,
         paddingRight: 10,
